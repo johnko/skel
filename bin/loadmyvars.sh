357a6c15e0b1c4185e41cf47593d14269ad50395
johnko <git@johnko.ca>
2015-11-21 01:09:29 -0500

    move cshvars to separate files

---
+++ bin/loadmyvars.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# used like this
+# . /usr/local/bin/loadmyvars.sh
+
+DEFAULTDIR=/root/perm
+LOCALDIR=/root/local
+
+for i in \
+    datamnt.conf \
+    fqdn.conf \
+    iocnet.conf \
+    iocpool.conf \
+    iocrelease.conf \
+    pkgpath.couchbase.conf \
+    pkgpath.ffmpeg.conf \
+    pkgpath.gitlab.conf \
+    pkgpath.lame.conf \
+    pkgpath.libaacplus.conf \
+    pkgpath.squid.conf \
+    squid.conf \
+    ; do
+    if [ -e ${DEFAULTDIR}/$i ]; then
+        . ${DEFAULTDIR}/$i
+    fi
+    if [ -e ${LOCALDIR}/$i ]; then
+        . ${LOCALDIR}/$i
+    fi
+done
